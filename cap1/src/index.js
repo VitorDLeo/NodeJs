f4c399a0a844dd40bbfb37616c945aaa7da28435
VitorDLeo <vitordileo003@gmail.com>
2022-05-17 18:34:08 -0300

    Estudando os metodos HTTP

--- cap1/src/index.js
+++ cap1/src/index.js
@@ -9,4 +9,22 @@ app.get("/courses", (request, response)=>{
     return response.json(["curso 1", "curso 2", "curso 3"]);
 });
 
+app.post("/courses", (request, response)=>{
+    return response.json(["curso 1", "curso 2", "curso 3"]);
+});
+
+app.put("/couse/:id", (request, response)=>{
+    return response.json(["curso 6", "curso 2", "curso 3"]);
+});
+
+app.patch("/course/:id", (request, response)=>{
+    return response.json(["curso 6", "curso 7", "curso 3"]);
+});
+
+app.delete("/course/:id", (request, response)=>{
+    return response.json(["curso 6", "curso 7"]);
+});
+
+
+
 app.listen(3333);
